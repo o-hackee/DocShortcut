o-hackee/DocShortcut

commit ec6ad09848ef7d52ef51c9beb9c430a86c9a79b5

adjust validity checks and updates

app/src/main/java/ol/ko/docshortcut/ui/FilePickerActivity.kt
@@ -14,16 +14,13 @@ import androidx.glance.appwidget.GlanceAppWidgetManager
 import androidx.glance.appwidget.state.updateAppWidgetState
 import androidx.glance.state.PreferencesGlanceStateDefinition
 import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import ol.ko.docshortcut.R
 import ol.ko.docshortcut.ShortcutGlanceWidget
 import ol.ko.docshortcut.ShortcutGlanceWidget.Companion.appWidgetIdPreferenceKey
 import ol.ko.docshortcut.ShortcutGlanceWidget.Companion.fileUriPreferenceKey
+import ol.ko.docshortcut.ShortcutGlanceWidget.Companion.isFileUriValidPreferenceKey
 import ol.ko.docshortcut.databinding.ActivityFilePickerBinding
-import ol.ko.docshortcut.utils.FileUrisDataStore
-import ol.ko.docshortcut.utils.FileUrisRepository
 
 class FilePickerActivity : AppCompatActivity() {
 
@@ -74,12 +71,9 @@ class FilePickerActivity : AppCompatActivity() {
 
         contentResolver.takePersistableUriPermission(fileUri, Intent.FLAG_GRANT_READ_URI_PERMISSION)
         val fileUriString = fileUri.toString()
-        lifecycleScope.launch {
-            FileUrisRepository(FileUrisDataStore.getInstance(this@FilePickerActivity)).saveUriPref(appWidgetId, fileUriString)
-        }
 
         // It is the responsibility of the configuration activity to update the app widget
-        CoroutineScope(Dispatchers.Main).launch {
+        lifecycleScope.launch {
             val context = this@FilePickerActivity
             val glanceId = GlanceAppWidgetManager(context).getGlanceIds(ShortcutGlanceWidget::class.java).lastOrNull()
 
@@ -107,7 +101,7 @@ class FilePickerActivity : AppCompatActivity() {
                         .apply {
                             this[appWidgetIdPreferenceKey] = appWidgetId
                             this[fileUriPreferenceKey] = fileUriString
-//                            this[isFileUriValidPreferenceKey] = true
+                            this[isFileUriValidPreferenceKey] = true
                         }
                 }
                 ShortcutGlanceWidget().update(context, glanceId)

app/src/main/java/ol/ko/docshortcut/RequestWidgetUpdate.kt
@@ -1,24 +1,55 @@
 package ol.ko.docshortcut
 
-import android.appwidget.AppWidgetManager
-import android.content.ComponentName
 import android.content.Context
-import android.content.Intent
+import android.util.Log
+import androidx.glance.GlanceId
+import androidx.glance.appwidget.GlanceAppWidgetManager
+import androidx.glance.appwidget.state.getAppWidgetState
+import androidx.glance.appwidget.state.updateAppWidgetState
+import androidx.glance.state.PreferencesGlanceStateDefinition
+import ol.ko.docshortcut.utils.ContentResolverUtils
 
-object WidgetUtils {
-    internal fun requestWidgetsUpdate(context: Context, appWidgetId: Int = AppWidgetManager.INVALID_APPWIDGET_ID) {
-        val appWidgetManager = AppWidgetManager.getInstance(context)
-        val appWidgetIds = if (appWidgetId == AppWidgetManager.INVALID_APPWIDGET_ID)
-            appWidgetManager.getAppWidgetIds(ComponentName(context, ShortcutAppWidget::class.java))
-        else
-            arrayOf(appWidgetId).toIntArray()
-        if (appWidgetIds.isNotEmpty()) {
-            context.sendBroadcast(
-                Intent(AppWidgetManager.ACTION_APPWIDGET_UPDATE).putExtra(
-                    AppWidgetManager.EXTRA_APPWIDGET_IDS,
-                    appWidgetIds
-                )
-            )
+// TODO rename the file in the next commit
+object GlanceWidgetUtils {
+
+    private const val TAG = "OLKO"
+
+    suspend fun updateWidget(context: Context, appWidgetId: Int) {
+        val glanceIds = GlanceAppWidgetManager(context).getGlanceIds(ShortcutGlanceWidget::class.java)
+        val glanceId = glanceIds.find {
+            val prefs = getAppWidgetState(context, PreferencesGlanceStateDefinition, it)
+            prefs[ShortcutGlanceWidget.appWidgetIdPreferenceKey] == appWidgetId
         }
+        glanceId?.let {
+            updateValidity(context, it)
+            ShortcutGlanceWidget().update(context, glanceId)
+        }
+    }
+
+    suspend fun updateWidgets(context: Context, onlyIfValidityChanged: Boolean = true) {
+        val glanceIds = GlanceAppWidgetManager(context).getGlanceIds(ShortcutGlanceWidget::class.java)
+        glanceIds.forEach { glanceId ->
+            val validityChanged = updateValidity(context, glanceId)
+
+            if (!onlyIfValidityChanged || validityChanged) {
+                ShortcutGlanceWidget().update(context, glanceId)
+            }
+        }
+    }
+
+    private suspend fun updateValidity(context: Context, glanceId: GlanceId): Boolean {
+        val prefs = getAppWidgetState(context, PreferencesGlanceStateDefinition, glanceId)
+        val fileUriString = prefs[ShortcutGlanceWidget.fileUriPreferenceKey]
+        val isCurrentlyValid = ContentResolverUtils.uriFileExists(context.contentResolver, fileUriString)
+        val validityChanged = isCurrentlyValid != prefs[ShortcutGlanceWidget.isFileUriValidPreferenceKey]
+        Log.d(TAG, "$glanceId: isCurrentlyValid $isCurrentlyValid validityChanged $validityChanged")
+        if (validityChanged) {
+            updateAppWidgetState(context, PreferencesGlanceStateDefinition, glanceId) { prefsToUpdate ->
+                prefsToUpdate.toMutablePreferences().apply {
+                    this[ShortcutGlanceWidget.isFileUriValidPreferenceKey] = isCurrentlyValid
+                }
+            }
+        }
+        return validityChanged
     }
 }

app/src/main/java/ol/ko/docshortcut/ShortcutAppWidget.kt
@@ -1,124 +1,0 @@
-package ol.ko.docshortcut
-
-import android.app.PendingIntent
-import android.appwidget.AppWidgetManager
-import android.appwidget.AppWidgetProvider
-import android.content.Context
-import android.os.Build
-import android.util.TypedValue
-import android.widget.RemoteViews
-import androidx.core.content.ContextCompat
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.launch
-import ol.ko.docshortcut.ui.MainActivity
-import ol.ko.docshortcut.utils.ContentResolverUtils
-import ol.ko.docshortcut.utils.FileUrisDataStore
-import ol.ko.docshortcut.utils.FileUrisRepository
-import ol.ko.docshortcut.work.FileCheckWorker
-
-/**
- * Implementation of App Widget functionality.
- */
-class ShortcutAppWidget : AppWidgetProvider() {
-
-    companion object {
-
-        internal fun updateAppWidget(
-            context: Context,
-            appWidgetManager: AppWidgetManager,
-            appWidgetId: Int,
-            fileUriString: String?,
-            isValid: Boolean = true
-        ) {
-            val fileName = fileUriString?.let {
-                ContentResolverUtils.getFilenameFromUri(
-                    context.contentResolver,
-                    it,
-                    context.getString(R.string.unknown_document)
-                )
-            } ?: context.getString(R.string.data_not_found)
-            // Construct the RemoteViews object
-            val views = RemoteViews(context.packageName, R.layout.shortcut_app_widget).apply {
-                with (R.id.file_uri) {
-                    setTextViewText(this, fileName)
-
-                    val color = if (isValid) {
-                        // couldn't obtain the color from the theme
-                        ContextCompat.getColor(context, R.color.on_primary)
-                    } else {
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                            val typedValue = TypedValue()
-                            context.theme.resolveAttribute(android.R.attr.colorError, typedValue, true)
-                            typedValue.data
-                        } else {
-                            ContextCompat.getColor(context, R.color.error)
-                        }
-                    }
-                    setTextColor(this, color)
-
-                    if (isValid) {
-                        setContentDescription(this, context.getString(R.string.appwidget_text))
-                    } else {
-                        setContentDescription(this, context.getString(R.string.appwidget_invalid_text))
-                    }
-                }
-                fileUriString?.let {
-                    setOnClickPendingIntent(
-                        R.id.container, PendingIntent.getActivity(
-                            context,
-                            MainActivity.PROXY_REQUEST + appWidgetId,
-                            MainActivity.createProxyIntent(context, it, appWidgetId),
-                            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
-                        )
-                    )
-                }
-            }
-
-//            println("updating")
-            // Instruct the widget manager to update the widget
-            appWidgetManager.updateAppWidget(appWidgetId, views)
-        }
-    }
-
-    override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
-        // There may be multiple widgets active, so update all of them
-        for (appWidgetId in appWidgetIds) {
-
-            val fileUrisSettings = FileUrisRepository(FileUrisDataStore.getInstance(context))
-            val fileUriStringFlow = fileUrisSettings.loadUriPref(appWidgetId)
-            CoroutineScope(Dispatchers.IO).launch {
-                fileUriStringFlow.collect { savedUriString ->
-                    val isCurrentlyValid = ContentResolverUtils.uriFileExists(context.contentResolver, savedUriString?.uriString)
-                    CoroutineScope(Dispatchers.Main).launch {
-                        updateAppWidget(context, appWidgetManager, appWidgetId, savedUriString?.uriString, isCurrentlyValid)
-                    }
-                    savedUriString?.let {
-                        if (isCurrentlyValid != it.lastIsValid) {
-                            fileUrisSettings.markUriPref(appWidgetId, isCurrentlyValid)
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    override fun onDeleted(context: Context, appWidgetIds: IntArray) {
-        for (appWidgetId in appWidgetIds) {
-            CoroutineScope(Dispatchers.Main).launch {
-                FileUrisRepository(FileUrisDataStore.getInstance(context)).deleteUriPref(appWidgetId)
-            }
-        }
-    }
-
-    override fun onEnabled(context: Context) {
-        super.onEnabled(context)
-        FileCheckWorker.start(context)
-    }
-
-    override fun onDisabled(context: Context) {
-        super.onDisabled(context)
-        FileCheckWorker.stop(context)
-    }
-}

app/src/main/java/ol/ko/docshortcut/ShortcutGlanceAppWidget.kt
@@ -49,12 +49,7 @@ class ShortcutGlanceWidgetReceiver : GlanceAppWidgetReceiver() {
 
     override val glanceAppWidget: GlanceAppWidget = ShortcutGlanceWidget()
 
-    override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
-        super.onUpdate(context, appWidgetManager, appWidgetIds)
-        Log.d(TAG, "ShortcutGlanceWidgetReceiver onUpdate()")
-        // TODO where belongs former markUriPref() functionality?
-    }
-
+    // TODO test this on the next stage
     override fun onEnabled(context: Context) {
         super.onEnabled(context)
 //        FileCheckWorker.start(context)
@@ -69,6 +64,9 @@ class ShortcutGlanceWidgetReceiver : GlanceAppWidgetReceiver() {
 class ShortcutGlanceWidget: GlanceAppWidget() {
 
     companion object {
+
+        private const val TAG = "OLKO"
+
         private const val fileUriKey = "fileuri-key"
         private const val appWidgetIdKey = "appwidgetid-key"
 
@@ -78,8 +76,6 @@ class ShortcutGlanceWidget: GlanceAppWidget() {
 
         val fileUriActionParameterKey = ActionParameters.Key<String>(fileUriKey)
         val appWidgetIdActionParameterKey = ActionParameters.Key<Int>(appWidgetIdKey)
-
-        const val DEFAULT_VALID = true
     }
 
     override val stateDefinition: GlanceStateDefinition<*> = PreferencesGlanceStateDefinition
@@ -96,7 +92,7 @@ class ShortcutGlanceWidget: GlanceAppWidget() {
         ) {
             val prefs = currentState<Preferences>()
             val fileUriString = prefs[fileUriPreferenceKey]
-            val isValid = prefs[isFileUriValidPreferenceKey] ?: DEFAULT_VALID
+            val isValid = prefs[isFileUriValidPreferenceKey] ?: true
 
             val context = LocalContext.current
             val fileName = fileUriString?.let {
@@ -133,6 +129,7 @@ class ShortcutGlanceWidget: GlanceAppWidget() {
                    )))
                }
             }
+            Log.d(TAG, "composing $fileName")
             Text(
                 text = fileName,
                 modifier = modifier,

app/src/main/java/ol/ko/docshortcut/ui/MainActivity.kt
@@ -11,9 +11,10 @@ import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
+import ol.ko.docshortcut.GlanceWidgetUtils
 import ol.ko.docshortcut.R
-import ol.ko.docshortcut.ShortcutAppWidget
-import ol.ko.docshortcut.WidgetUtils
 import ol.ko.docshortcut.databinding.ActivityMainBinding
 import java.io.FileNotFoundException
 
@@ -22,7 +23,6 @@ class MainActivity : AppCompatActivity() {
     companion object {
         private const val TAG = "OLKO"
 
-        const val PROXY_REQUEST = 1
         const val EXTRA_PROXY_REQUEST_KEY = "PROXY_REQUEST"
         const val EXTRA_URI_KEY = "URI"
 
@@ -59,9 +59,11 @@ class MainActivity : AppCompatActivity() {
         super.onStart()
         Log.d(TAG, "onStart ${intent.action} ${intent.extras}")
 
-        // the problem is steadily observed on API 29 emulator and sporadically - on real device
-        WidgetUtils.requestWidgetsUpdate(this)
-//            Toast.makeText(context, R.string.updating, Toast.LENGTH_SHORT).show()
+        // the problem with stale UI ("Tap to update", i.e. appwidget_text)
+        // is steadily observed on API 29 emulator and sporadically - on real device
+        lifecycleScope.launch {
+            GlanceWidgetUtils.updateWidgets(this@MainActivity, onlyIfValidityChanged = false)
+        }
     }
 
     private fun close() {
@@ -108,7 +110,8 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, "An error occurred while trying to open $fileUriString", Toast.LENGTH_LONG).show()
         }
 
-        // TODO make sure this works for glance widgets
-        WidgetUtils.requestWidgetsUpdate(this, appWidgetId)
+        lifecycleScope.launch {
+            GlanceWidgetUtils.updateWidget(this@MainActivity, appWidgetId)
+        }
     }
 }

app/src/main/java/ol/ko/docshortcut/utils/PreferencesUtil.kt
@@ -1,96 +1,0 @@
-package ol.ko.docshortcut.utils
-
-import android.content.Context
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.core.stringSetPreferencesKey
-import androidx.datastore.preferences.preferencesDataStore
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.map
-import ol.ko.docshortcut.utils.FileUrisRepository.Companion.INVALID_MARK
-
-data class SavedUriString(
-    val uriString: String,
-    val lastIsValid: Boolean
-)
-
-object FileUrisDataStore {
-    private const val PREFS_NAME = "file_uris"
-    private val Context.prefsDataStore: DataStore<Preferences> by preferencesDataStore(PREFS_NAME)
-
-    fun getInstance(context: Context) = context.prefsDataStore
-}
-
-class FileUrisRepository(
-    private val dataStore: DataStore<Preferences>
-) {
-    companion object {
-        const val PREF_PREFIX_KEY = "appwidget_"
-        const val INVALID_MARK = "INVALID"
-    }
-
-    private fun keyFileUri(appWidgetId: Int) = stringSetPreferencesKey(PREF_PREFIX_KEY + appWidgetId)
-
-    internal suspend fun saveUriPref(appWidgetId: Int, fileUri: String) {
-        dataStore.edit { prefs ->
-            prefs[keyFileUri(appWidgetId)] = setOf(fileUri)
-//            println("saved $appWidgetId $fileUri")
-        }
-    }
-
-    internal suspend fun markUriPref(appWidgetId: Int, asValid: Boolean) {
-        dataStore.edit { prefs ->
-            val stringsValue = prefs[keyFileUri(appWidgetId)]
-            stringsValue.fileStringUri()?.let {
-                prefs[keyFileUri(appWidgetId)] = if (asValid) setOf(it) else setOf(it, INVALID_MARK)
-//                println("marked $appWidgetId $asValid")
-            }
-        }
-    }
-
-    internal fun loadUriPref(appWidgetId: Int): Flow<SavedUriString?> = dataStore.data.map { prefs ->
-        val stringsValue = prefs[keyFileUri(appWidgetId)]
-//        println("loaded $appWidgetId $stringsValue")
-        stringsValue.toSavedUriString()
-    }
-
-    internal suspend fun deleteUriPref(appWidgetId: Int) {
-        dataStore.edit { prefs ->
-            prefs.minusAssign(keyFileUri(appWidgetId))
-        }
-    }
-
-    @Suppress("UNCHECKED_CAST")
-    internal suspend fun allUriPrefs(): List<Pair<Int?, SavedUriString?>> {
-        val prefs = try {
-            dataStore.data.first().asMap()
-        } catch (_: NoSuchElementException) {
-            return emptyList()
-        }
-        return prefs.mapNotNull { entry ->
-            if (entry.key.name.startsWith(PREF_PREFIX_KEY)) {
-                entry.key.name.substring(PREF_PREFIX_KEY.length).toIntOrNull() to (entry.value as? Set<String>).toSavedUriString()
-            } else
-                null
-        }
-    }
-}
-
-fun Set<String>?.fileStringUri(): String? {
-    return this?.let { strings ->
-        if (strings.count() == 1) strings.firstOrNull() else {
-            strings.firstOrNull { it != INVALID_MARK }
-        }
-    }
-}
-
-fun Set<String>?.toSavedUriString(): SavedUriString? {
-    return this?.let { strings ->
-        val fileStringUri = fileStringUri()
-        fileStringUri?.let {
-            SavedUriString(fileStringUri, strings.count() == 1)
-        }
-    }
-}

app/src/main/java/ol/ko/docshortcut/work/FileCheckWorker.kt
@@ -1,26 +1,18 @@
 package ol.ko.docshortcut.work
 
-import android.appwidget.AppWidgetManager
 import android.content.Context
-import android.util.Log
 import androidx.work.Constraints
 import androidx.work.CoroutineWorker
 import androidx.work.ExistingPeriodicWorkPolicy
 import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
 import androidx.work.WorkerParameters
-import ol.ko.docshortcut.ShortcutAppWidget
-import ol.ko.docshortcut.WidgetUtils
-import ol.ko.docshortcut.utils.ContentResolverUtils
-import ol.ko.docshortcut.utils.FileUrisDataStore
-import ol.ko.docshortcut.utils.FileUrisRepository
+import ol.ko.docshortcut.GlanceWidgetUtils
 import java.util.concurrent.TimeUnit
 
 class FileCheckWorker(appContext: Context, workerParams: WorkerParameters) :
     CoroutineWorker(appContext, workerParams) {
     companion object {
-        private const val TAG = "OLKO"
-
         private const val workName = "fileCheckWorker"
         private const val repeatIntervalInDays = 1L
         private const val flexIntervalInHours = 1L
@@ -50,24 +42,7 @@ class FileCheckWorker(appContext: Context, workerParams: WorkerParameters) :
     }
 
     override suspend fun doWork(): Result {
-        val fileUrisRepository = FileUrisRepository(FileUrisDataStore.getInstance(applicationContext))
-        val uris = fileUrisRepository.allUriPrefs()
-//        println("uris: $uris")
-        uris.forEach { pair ->
-            pair.first?.let { appWidgetId ->
-                val savedUriString = pair.second
-                savedUriString?.let {
-                    val isCurrentlyValid = ContentResolverUtils.uriFileExists(applicationContext.contentResolver, savedUriString.uriString)
-                    Log.d(TAG, "$appWidgetId: $isCurrentlyValid")
-//                    println("$appWidgetId: $isCurrentlyValid")
-                    if (isCurrentlyValid != it.lastIsValid) {
-                        // onUpdate() will do markUriPref()
-                        WidgetUtils.requestWidgetsUpdate(applicationContext, appWidgetId)
-                    }
-                }
-            }
-        }
-//        println("return doWork")
+        GlanceWidgetUtils.updateWidgets(applicationContext)
         return Result.success()
     }
 }

app/src/main/res/values/strings.xml
@@ -8,7 +8,6 @@
     <string name="main_activity_prompt">Use system Widgets tool to set up your shortcuts</string>
     <string name="unknown_document">Unknown document</string>
     <string name="close_main">Close</string>
-    <string name="updating">Updating your widgets…</string>
     <string name="about">About</string>
     <string name="about_text">Minimalistic utility to create document shortcuts as app widgets\
         \

commit 8602f1a5cd4e13b952065ea89202e6f45f90e095

fix wrong switching to lifecycleScope in FilePickerActivity introduced in ec6ad09 (adjust validity checks and updates, 2022-02-11)

app/src/main/java/ol/ko/docshortcut/ui/FilePickerActivity.kt
@@ -9,7 +9,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import ol.ko.docshortcut.GlanceWidgetUtils
 import ol.ko.docshortcut.R
@@ -66,7 +67,7 @@ class FilePickerActivity : AppCompatActivity() {
         val fileUriString = fileUri.toString()
 
         // It is the responsibility of the configuration activity to update the app widget
-        lifecycleScope.launch {
+        CoroutineScope(Dispatchers.Main).launch { // lifecycleScope would not work: the job is likely to be canceled when the activity is finished i.e. very soon
             GlanceWidgetUtils.fillInitialWidgetState(this@FilePickerActivity, appWidgetId, fileUriString)
         }
 

app/src/androidTest/java/ol/ko/docshortcut/UiTest.kt
@@ -213,21 +213,21 @@ open class FilesBaseTest: WidgetsBaseTest() {
         button.click()
 
         filePickerNavigateToTestDocument(fileName, targetFolderLink, targetFolderName, useProviderLink, fromRemovableStorage).click()
-        val widgetView = getAddedWidget(fileName)
+        val widgetView = getAddedWidget(fileName, withDescription = false) // Glance doesn't support contentDescription for text
         assertNotNull(widgetView)
         return widgetView!!
     }
 
     private fun getAddedWidget(fileName: String, withDescription: Boolean = true): UiObject2? {
-        val addedWidgets = device.wait(Until.findObjects(By.desc(widgetLabel)), ACTION_TIMEOUT)
-        assertNotEquals(0, addedWidgets.size)
+            val addedWidgets = device.wait(Until.findObjects(By.desc(widgetLabel)), ACTION_TIMEOUT)
+            assertNotEquals(0, addedWidgets.size)
         return addedWidgets.find {
-            val selector = By.clazz(TextView::class.java).text(fileName)
-            if (withDescription) {
-                selector.desc(targetContext.getString(R.string.appwidget_text))
+                val selector = By.clazz(TextView::class.java).text(fileName)
+                if (withDescription) {
+                    selector.desc(targetContext.getString(R.string.appwidget_text))
+                }
+                it.hasObject(selector)
             }
-            it.hasObject(selector)
-        }
     }
 
     protected fun filePickerNavigateToTestDocument(fileName: String, targetFolderLink: String, targetFolderName: String, useProviderLink: Boolean, fromRemovableStorage: Boolean): UiObject2 {
@@ -349,7 +349,6 @@ open class DocumentsBaseTest: FilesBaseTest() {
     }
 }
 
-@Ignore
 @RunWith(AndroidJUnit4::class)
 class DocumentsUiTest: DocumentsBaseTest() {
 